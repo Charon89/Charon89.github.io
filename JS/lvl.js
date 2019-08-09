10feea4bd9d81c1edac7ac02719c3d151e629d3b
Dmytro Andriushchenko <46615932+Charon89@users.noreply.github.com>
2019-08-09 10:34:34 -0400

    First Upload

---
+++ JS/lvl.js
@@ -0,0 +1,260 @@
+let balls_amount = localStorage.getItem("balls_amount");
+let time = localStorage.getItem("time_lvl1");;
+let random_gen = rnd();
+let click_counter = 0;
+let expression_result;
+
+let int = setInterval(myTimer, 1000);
+
+let score_container = document.querySelector(".score_container");
+let curson_container = document.querySelector(".curson_container");
+
+let score_value = document.querySelector(".score_value");
+let attemps_value = document.querySelector(".attemps_value");
+let correct_value = document.querySelector(".correct_value");
+let wrong_value = document.querySelector(".wrong_value");
+let view = document.querySelector(".playground");
+let timer = document.querySelector(".time_value");
+let pop_sound = document.querySelector("#pop_sound");
+
+let operand_1 = document.querySelector("#operand_1");
+let operand_2 = document.querySelector("#operand_2");
+attemps_value.innerHTML = "4";
+//---- FULLSCREEN MODE
+let fs_status = 0;
+let fs = document.querySelector("#fs_button");
+function toggleFullScreen() {
+
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    fs.src = "../IMG/buttons/button_FullScreen.png";
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+    fs.src = "../IMG/buttons/button_FullScreen_pressed.png";
+
+  }
+}
+fs.addEventListener("click", toggleFullScreen);
+
+class balloon {
+  constructor(source, width, value) {
+    this.img_source = source;
+    this.div_box_width = width;
+    this.p_value = value;
+  }
+
+  create() {
+    let newpos = makeNewPosition();
+    let newDiv = document.createElement('div');
+    let newImg = document.createElement('img');
+    let newP = document.createElement('p');
+
+    //newDiv.setAttribute("id","crosss_"+val);
+    newDiv.style.width = this.div_box_width + 'px';
+    newImg.style.width = this.div_box_width + 'px';
+    newP.innerHTML = this.p_value;
+
+
+    newP.style.width = this.div_box_width + 'px';
+    newP.style.padding = "30%";
+    newP.style.zIndex = 2;
+    newP.style.textAlign = "center";
+    newP.style.color = "yellow";
+    newP.style.position = "absolute";
+
+
+    newDiv.style.position = "absolute";
+    newDiv.style.top = 300+'px';
+    newDiv.style.left = -300+'px';
+    //alert("H: " + newpos[0] + " W: " + newpos[1] );
+
+    newImg.src = this.img_source;
+    newImg.style.position = "relative";
+    newImg.style.top = newpos[0];
+    newImg.style.left = newpos[1];
+
+  
+    let container = document.querySelector(".playground"); 
+    let newnewDiv = container.appendChild(newDiv);
+    let newnewImg = newDiv.appendChild(newP);
+    newnewDiv.appendChild(newImg);
+
+    newDiv.className = "newBalloon";
+    newP.className = "newParagraph";
+
+  }
+  changeRandom( random ){
+    let pRandomValue = random;
+    let changeP = document.querySelector('.newParagraph');
+    changeP.innerHTML = pRandomValue;
+   // alert(this.p_value);
+  }
+  getValue(){
+    return this.p_value;
+  }
+  modifyValue(a){
+    this.p_value = a;
+  }
+}
+
+function rnd(){
+  return Math.floor(Math.random() * 34 + 1);
+}
+
+function randon_expession() {
+  operand_1.innerHTML = Math.floor(Math.random() * 25 + 1);
+  operand_2.innerHTML = Math.floor(Math.random() * 9 + 1);
+  $('.newBalloon').innerHTML = parseInt(operand_1.innerHTML) + parseInt(operand_2.innerHTML);
+  expression_result = parseInt(operand_1.innerHTML) + parseInt(operand_2.innerHTML);
+  document.querySelector(".newParagraph").innerHTML = expression_result;
+  //document.querySelector(".newParagraph").style.color = "red";
+}
+//window.addEventListener("load",randon_expession);
+
+
+var ball = [];
+operand_1.innerHTML = Math.floor(Math.random() * 25 + 1);
+operand_2.innerHTML = Math.floor(Math.random() * 9 + 1);
+for (let i = 0; i < balls_amount; i++) {
+  let rend_p = makeNewPosition();
+  random_gen = Math.floor(Math.random() * 34 + 1);
+  ball[i] = new balloon("../IMG/balloons/ball_"+Math.floor(Math.random() * 5 + 1)+".png", 100, rnd());
+  expression_result = parseInt(operand_1.innerHTML) + parseInt(operand_2.innerHTML);
+  ball[0].modifyValue(parseInt(expression_result));
+  ball[i].create();
+  $('.newBalloon').innerHTML = parseInt(operand_1.innerHTML) + parseInt(operand_2.innerHTML);
+  document.querySelector(".newParagraph").innerHTML = expression_result;
+  //document.querySelector(".newParagraph").style.color = "red";
+}
+
+//---- Balloons move
+$(document).ready(function () {
+  animateDiv('.newBalloon');
+});
+
+function makeNewPosition() {
+  let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) - 100;
+  let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 100;
+  let h_head = document.querySelector("#head_name").offsetHeight;
+  let nh = Math.floor(Math.random() * (h - h_head));
+  let nw = Math.floor(Math.random() * w);
+
+  return [nh, nw];
+}
+
+function animateDiv(myclass) {
+  $('.newBalloon').each(function(){
+    let newq = makeNewPosition();
+  $(this).animate({ top: newq[0], left: newq[1],opacity: 1, queue: false }, 3000, 
+  function () {
+    animateDiv(myclass);
+  });
+  });
+}
+
+function playAudio() { 
+  pop_sound.play(); 
+} 
+
+let if_end = setInterval(endCheck, 100);
+  function endCheck() {
+      if(parseInt(attemps_value.innerHTML) == 0){
+        endLevel();
+      }
+  }
+  window.addEventListener("load",endCheck);
+
+//   $(document).on('click', '.playground', function(){ 
+//     $(document).on('click', '.newBalloon', function(){ 
+//         alert($(this).find('.newParagraph').text());
+//     });       
+// });
+  
+ function balloonDoAction(){
+     var x = $(".newBalloon");
+       for (var j = 0; j < x.length; j++) {
+         x[j].addEventListener('click', function(event) {
+          let inner_value = parseInt($(this).find('.newParagraph').text())
+          if(inner_value == expression_result)
+          {
+            click_counter++;
+            score_value.innerHTML = click_counter;
+            correct_value.innerHTML = parseInt(correct_value.innerHTML)+1;
+            attemps_value.innerHTML = parseInt(attemps_value.innerHTML)-1;
+            $(".newBalloon").remove();
+            playAudio();
+            respawn();
+            randon_expession();
+          }
+          else{
+            click_counter++;
+            score_value.innerHTML = click_counter;
+            attemps_value.innerHTML = parseInt(attemps_value.innerHTML)-1;
+            wrong_value.innerHTML = parseInt(wrong_value.innerHTML)+1;
+            $(this).remove();
+            playAudio();
+          }
+        });
+      }   
+  }
+  $('.playground').on("click", "div.newBalloon", balloonDoAction);
+//document.querySelector(".newBalloon").addEventListener("click", balloonDoAction);
+  
+  function respawn() {
+   // alert(popped);
+      ball = [];
+      for (let i = 0; i < balls_amount; i++) {
+      let rend_p = makeNewPosition();
+      ball[i] = new balloon("../IMG/balloons/ball_"+Math.floor(Math.random() * 5 + 1)+".png", 100, rnd());
+      ball[i].create();
+    }
+  }
+
+
+//Score-Attemps-Hits
+
+function endLevel(){
+  clearInterval(int);
+  clearInterval(if_end);
+  //timer.innerHTML = "Time is up!";
+      score_container.style.padding = "10%";
+      score_container.style.paddingTop = "0px";
+      score_container.style.textAlign = "center";
+      score_container.style.width = "100vw";
+      score_container.style.height = "100vh";
+      view.style.display = "none";
+      let next = document.querySelector("#next_lvl");
+      next.style.display = "inline";
+}
+
+//MOUSE COORDS
+function curson_container_move(event) {
+  let mouse_y = event.clientY + 5;
+  let mouse_x = event.clientX + 5;
+  curson_container.style.top = mouse_y + "px";
+  curson_container.style.left = mouse_x + "px";
+}
+document.addEventListener('mousemove', curson_container_move);
+
+function myTimer() {
+    if (time > 0)
+      timer.innerHTML = time--;
+    if (time < 10)
+      timer.style.color = "red";
+    if (time <= 0) {
+      endLevel();
+      //clearInterval(myTimer);
+    }
+}
+
+//On load events
+
+
+window.addEventListener("load", myTimer);
---
+++ JS/script.js
@@ -0,0 +1,106 @@
+localStorage.setItem("balls_amount",9);
+localStorage.setItem("time_lvl1",90);
+localStorage.setItem("time_lvl2",120);
+localStorage.setItem("time_lvl3",150);
+
+let getName_container = document.querySelector(".getName_container");
+window.addEventListener("load",()=>{
+  getName_container.style.display="block";
+});
+
+
+function getName_container_hide(){
+  getName_container.style.display="none";
+}
+
+//fetch_name.addEventListener("click", getName_container_hide);
+submit_name=()=>{
+  let good = false;
+  let name_form = document.getElementsByClassName("getName_form");  
+
+  
+  let age = parseInt(name_form[1].value);
+  let name = name_form[0].value;
+
+      if((age < 50 && age > 3) && (age !="") && name.length > 2)
+      {
+        getName_container_hide();
+      }
+      else{
+        //alert("age: "+age + "Name length: "+ name.length);
+        alert("Invalid entry");
+      }
+
+  localStorage.setItem("name",name_form[0].value);
+  localStorage.setItem("age",name_form[1].value);
+}
+
+
+document.querySelector("#getName_button").addEventListener("click",submit_name);
+
+//CREATED BY MOZILLA ---- FULLSCREEN MODE
+let fs_status = 0;
+let fs = document.querySelector("#fs_button");
+function toggleFullScreen() {
+
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    fs.src = "IMG/buttons/button_FullScreen.png";
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+    fs.src = "IMG/buttons/button_FullScreen_pressed.png";
+
+  }
+}
+fs.addEventListener("click", toggleFullScreen);
+
+
+//Select lvl************
+let lvl_status = 0;
+
+let lvl1 = document.querySelector("#lvl1"); //img
+let lvl2 = document.querySelector("#lvl2"); //img
+let lvl3 = document.querySelector("#lvl3"); //img
+
+let start = document.querySelector("#start");
+
+lvl1.addEventListener("click", () => lvl_status = 1);
+lvl2.addEventListener("click", () => lvl_status = 2);
+lvl3.addEventListener("click", () => lvl_status = 3);
+
+function change_button() {
+
+  if (lvl_status == 1) {
+    lvl1.src = "IMG/buttons/pressed_button_fuzzy_1_text.png";
+    lvl2.src = "IMG/buttons/button_fuzzy_2_text.png";
+    lvl3.src = "IMG/buttons/button_fuzzy_3_text.png";
+    start.href = "HTML/lvl1.html";
+  }
+
+  if (lvl_status == 2) {
+    lvl1.src = "IMG/buttons/button_fuzzy_1_text.png";
+    lvl2.src = "IMG/buttons/pressed_button_fuzzy_2_text.png";
+    lvl3.src = "IMG/buttons/button_fuzzy_3_text.png";
+    start.href = "HTML/lvl2.html";
+  }
+
+  if (lvl_status == 3) {
+    lvl1.src = "IMG/buttons/button_fuzzy_1_text.png";
+    lvl2.src = "IMG/buttons/button_fuzzy_2_text.png";
+    lvl3.src = "IMG/buttons/pressed_button_fuzzy_3_text.png";
+    start.href = "HTML/lvl3.html";
+  }
+}
+
+lvl1.addEventListener("click", change_button);
+lvl2.addEventListener("click", change_button);
+lvl3.addEventListener("click", change_button);
+
+//Start****************
